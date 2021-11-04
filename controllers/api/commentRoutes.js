8d13bbb08cc3c2d6264f3ba293cbda076f7d5eb3
James Strande <jamesstrande@MacBook-Pro.fios-router.home>
2021-11-04 03:58:17 -0400

    added comment route and with auth

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/withAuth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body
+        });
+        res.status(200).json(newComment)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./commentRoutes.js');
+const postRoutes = require('./postRoutes.js');
 
 router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+router.use('/post', postRoutes);
 
 module.exports = router;
