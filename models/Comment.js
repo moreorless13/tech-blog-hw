7aa98aafb7574ecf7004b05e0a46c92b08497507
James Strande <jamesstrande@MacBook-Pro.fios-router.home>
2021-11-04 03:45:54 -0400

    post and comment models and model index

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const { User, Post } = require('../models');
+const sequelize = require('../config/config');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, 
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }, 
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Comment',
+    }
+)
+
+module.exports = Comment;
