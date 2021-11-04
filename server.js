6cd5db0134c4fad775e5b32798cab7aa44d6894d
James Strande <strandejr@gmail.com>
2021-11-04 00:05:46 -0400

    added in session with config, specified to use handlebars and to utilize the public directory to handle front end and controllers to handle routing

--- server.js
+++ server.js
@@ -24,6 +24,26 @@ const sess = {
     })
 };
 
+// tell express to use the session configs
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// tell express where routing exists
+
+app.use(require('./controllers/'))
+
+// start the server by telling which port to listen to 
 app.listen(PORT, () => {
     console.log(`App listening on port localhost:${PORT}`);
     sequelize.sync({ force: false });
