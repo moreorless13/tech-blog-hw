0b2f9c7f660a1f0abbc6b40e3394846ea250f752
James Strande <strandejr@gmail.com>
2021-11-04 15:23:44 -0400

    homeroutes completed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,60 +1,68 @@
 const router = require('express').Router();
-const { User, Post } = require('../models/');
+const { User, Post, Comment } = require('../models/');
 const withAuth = require('../utils/withAuth');
 
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-            ],
+            include: [User]
         });
 
         const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('home', {
-            posts,
-            logged_in: req.session.logged_in
-        })
+        res.render('all-posts', { posts })
     } catch (error) {
         res.status(500).json(error)
     }
 
 })
 
-router.get('/dashboard', withAuth, async (req, res) => {
-    try {
-        const userData = await User.findByPk(req.body.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{ model: Post }]
-        });
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
 
-        const user = userData.get({ plain: true });
-        res.render('dashboard', {
-            ...user,
-            logged_in: true
-        });
-    } catch (error) {
-        res.status(500).json(error);
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('single-post', { post });
+    } else {
+      res.status(404).end();
     }
-})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
 router.get('/login', async (req, res) => {
     try {
-        if (req.body.logged_in) {
-            res.redirect('/dashboard');
+        if (req.session.loggedIn) {
+            res.redirect('/');
             return;
         }
 
-        res.render('dashboard');
+        res.render('login');
     } catch (error) {
         res.status(500).json(error)
     }
 
 });
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+})
 
 module.exports = router;
